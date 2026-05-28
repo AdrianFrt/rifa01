bd4868e0c0336ede08978ce39a0128d69f7eb21b
Adrian Ferraz <ferrazadrian102@gmail.com>
2026-05-28 11:09:22 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,4 +1,22 @@
 var ul = document.getElementById('ul')
+
+var rifa = document.querySelectorAll('.rifa')
+
+var animarEobservar = new IntersectionObserver((elemento1) => {
+
+    elemento1.forEach((elemento2) => {
+
+        if(elemento2.isIntersecting === true){
+            elemento2.target.classList.add('ativar')
+        }else {
+            elemento2.target.classList.remove('ativar')
+        }
+    })
+})
+
+rifa.forEach((finalizar) => {
+    animarEobservar.observe(finalizar)
+})
 function menu(){
 
     ul.classList.toggle('ativar')

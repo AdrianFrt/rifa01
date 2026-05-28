618d9f70db1312f80bd0a40065666cd2a170579b
Adrian Ferraz <ferrazadrian102@gmail.com>
2026-05-27 21:36:15 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,7 +1,14 @@
 var ul = document.getElementById('ul')
-
+var botoes = document.querySelectorAll('.rifa')
 
 function menu(){
 
     ul.classList.toggle('ativar')
 }
+
+botoes.forEach(function(botao){
+
+    botao.onclick = function(){
+        window.location.href = 'https://invoice.infinitepay.io/adrian-ferraz-k65/Xl4jg35Rig'
+    }
+})
